97059432e4b6880c08e08705f6dee1621218d5c4
VASILY MEDVEDEV <medvedevvu72@gmail.com>
2020-04-12 21:26:57 +0400

    cicle1 Dollar - add struct and methods

--- parti/types.go
+++ parti/types.go
@@ -1,2 +1,16 @@
 package parti
 
+//Dollar
+type Dollar struct {
+	amount int
+}
+
+//Dollar constructor
+func NewDollar(amount int) *Dollar {
+	return nil
+}
+
+// Multyplication
+func (d *Dollar) times(tm int) {
+
+}
