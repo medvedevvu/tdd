222e0abb26a4efb207a0dbe895f4f8ca7222c3fb
VASILY MEDVEDEV <medvedevvu72@gmail.com>
2020-04-12 21:16:54 +0400

    init Dollar

---
+++ parti/types_test.go
@@ -0,0 +1,13 @@
+package parti
+
+import (
+	"testing"
+)
+
+func TestMultiplication(t *testing.T) {
+	five := NewDollar(5.0)
+	(*five).times(2)
+	if (*five).amount != 10.0 {
+		t.Fatalf(" not equal 10 ")
+	}
+}
